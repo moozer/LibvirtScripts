c5c34dea5cc70d6a3e9333dabc524b15dc3bcfe7
moz <moz@local>
2013-09-05 16:02:55 +0200

    domain and image extraction done

--- TempSrv/CloneToTemp.sh
+++ TempSrv/CloneToTemp.sh
@@ -10,24 +10,30 @@ VMTOCOPY=$1
 
 CURDATE=$(date +"%y%m%d_%H%M")
 OLDXMLFILE="tmp_$CURDATE.xml"
-sudo virsh dumpxml $VMTOCOPY
+
+# get the xml
+sudo virsh dumpxml $VMTOCOPY > $OLDXMLFILE
+if [ $? -ne 0 ]; then
+	echo something went wrong getting the xml
+	exit 2
+fi
 
 # vars
 NEWDOMAIN="$1_$CURDATE"
-OLDIMG=$(cat $OLDXMLFILE | sed "s#<source file='\(.*\)'/>#\1#" | sed -e 's/^[ \t]*//') # find data, then trim spaces.
-NEWIMG="$OLDIMG_$CURDATE.qcow2.img"
+OLDIMG=$(cat $OLDXMLFILE | sed -ne "s#<source file='\(.*\)'/>#\1#p" | grep -v iso | sed 's/^ *//g') # find data, then skip all iso, then trim.
+NEWIMG="$(dirname $OLDIMG)/$(basename $OLDIMG '.img')_$CURDATE.img"
 
-
-echo Cloning virtual machine $VMTOCOPY
-echo - will create overlayed image $NEWIMG
-echo - base image i $OLDIMG
-echo - new domain is $NEWDOMAIN
+echo "Cloning virtual machine $VMTOCOPY"
+echo "- base image is $OLDIMG"
+echo "- will create overlayed image $NEWIMG"
+echo "- new domain is $NEWDOMAIN"
 
 
 echo will run
 echo ./CloneVm.sh $VMTOCOPY $NEWDOMAIN $NEWIMG
 
-rm $OLDXMLFILE
+echo Cleaning up
+#rm $OLDXMLFILE
 
 # yeah! success
 exit 0
