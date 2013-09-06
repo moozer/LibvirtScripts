6d123d0ca2da034163a3d5fd9c608934d8774109
moz <moz@local>
2013-09-06 14:55:26 +0200

    Added PromoteTemp

---
+++ TempSrv/PromoteTemp.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+	echo "usage: $0 <vm-to-promote> <new-name>"
+	exit 1
+fi
+VMTOPROM=$1
+NEWNAME=$2
+
+OLDIMG=$(sudo virsh dumpxml $VMTOPROM | sed -ne "s#<source file='\(.*\)'/>#\1#p1" | head -n 1 | sed 's/^ *//g') # find data, then pick first line, then trim.
+NEWIMG="$(dirname $OLDIMG)/$2.img"
+
+echo Promoting VM $VMTOPROM
+echo - Will flatten image $OLDIMG
+echo - New image name $NEWIMG
+echo - will rename $VMTOPROM to $NEWNAME
+echo
+echo Note: Not deleting old VM.
+
+echo
+echo Flattening
+echo ./ImgFlatten.sh $OLDIMG $NEWIMG
+
+echo
+echo Creating new machine
+echo ./CloneVm.sh $VMTOPROM $NEWNAME $NEWIMG
+
+
+
