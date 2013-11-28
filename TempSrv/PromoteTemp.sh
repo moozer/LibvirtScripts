4fd117465bf8f18388a06c82f1a3d1344f1f0748
moz <moz@local>
2013-11-28 11:20:43 +0100

    Promotetemp now using realpath also

--- TempSrv/PromoteTemp.sh
+++ TempSrv/PromoteTemp.sh
@@ -7,6 +7,14 @@ fi
 VMTOPROM=$1
 NEWNAME=$2
 
+if ! which realpath > /dev/null; 
+then 
+	echo "realpath program not found in PATH"
+	echo "user 'apt-get install realpath' or similar"
+	exit
+fi
+PROGDIR=$(dirname $(realpath $0))
+
 OLDIMG=$(sudo virsh dumpxml $VMTOPROM | sed -ne "s#<source file='\(.*\)'/>#\1#p1" | head -n 1 | sed 's/^ *//g') # find data, then pick first line, then trim.
 NEWIMG="$(dirname $OLDIMG)/$2.img"
 
@@ -19,11 +27,11 @@ echo Note: Not deleting old VM.
 
 echo
 echo Flattening
-./ImgFlatten.sh $OLDIMG $NEWIMG
+$PROGDIR/ImgFlatten.sh $OLDIMG $NEWIMG
 
 echo
 echo Creating new machine
-./CloneVm.sh $VMTOPROM $NEWNAME $NEWIMG
+$PROGDIR/CloneVm.sh $VMTOPROM $NEWNAME $NEWIMG
 
 
 
